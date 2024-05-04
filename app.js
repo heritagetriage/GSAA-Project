578b14e6ef6ce0b2a1f4b39cbd194fda079ae7bf
Nanasefa82 <142455750+Nanasefa82@users.noreply.github.com>
2024-05-04 16:16:18 -0400

    make it better

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//Require modules
 const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
@@ -11,67 +10,54 @@ const eventRoutes = require('./routes/eventRoute');
 const userRoutes = require('./routes/userRoute');
 const feedbackRoutes = require('./routes/feedbackRoute');
 const { error } = require('console');
-const {validateId} = require('./middleware/validator')
+const { validateId } = require('./middleware/validator');
 
-//create app
 const app = express();
 
-
-//Configure application
-let port = 5000;
-let host = 'localhost';
+// Configure application
 app.set('view engine', 'ejs');
 
+// Connect to database
+const dbURI = 'mongodb+srv://nanasefa:kings1982@cluster0.pyciqy4.mongodb.net/project3?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(process.env.PORT || 5000, () => {
+            console.log('Server is running on port', process.env.PORT || 5000);
+        });
+    })
+    .catch(err => console.error(err));
 
-//Connect to database
-mongoose.connect('mongodb+srv://nanasefa:kings1982@cluster0.pyciqy4.mongodb.net/project3?retryWrites=true&w=majority',
-                   )
-.then(()=>{
-    //start the server
-    app.listen(port, host, ()=> {
-    console.log('Server is running on port', port);
-});
-})
-.catch(err=>console.log(err.message));
+// Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+app.use(methodOverride('_method'));
 
-//mount middlware
-app.use(
-    session({
-        secret: "ajfeirf90aeu9eroejfoefj",
-        resave: false,
-        saveUninitialized: false,
-        store: new MongoStore({mongoUrl: 'mongodb+srv://nanasefa:kings1982@cluster0.pyciqy4.mongodb.net/project3?retryWrites=true&w=majority'}),
-        cookie: {maxAge: 60*60*1000}
-        })
-);
+app.use(session({
+    secret: "ajfeirf90aeu9eroejfoefj",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: dbURI }),
+    cookie: { maxAge: 60 * 60 * 1000 }
+}));
 app.use(flash());
 
-
-
 app.use((req, res, next) => {
-    // console.log(req.session);
-    res.locals.user = req.session.user||null;
+    res.locals.user = req.session.user || null;
     res.locals.errorMessages = req.flash('error');
     res.locals.successMessages = req.flash('success');
     next();
 });
 
-// Mount Middleware
-app.use(express.static('public'));
-app.use(express.urlencoded({extended :true}));
-app.use(morgan('tiny'));
-app.use(methodOverride('_method'));
-
+// Routes
 app.use('/', mainRoutes);
-
 app.use('/events', eventRoutes);
 app.use('/users', userRoutes);
-app.use('/feedbacks', feedbackRoutes); // Corrected path
+app.use('/feedbacks', feedbackRoutes);
 
-
-// Cannot find a resource Error Page
-app.use((req, res, next)=>{
-    let err = new Error('The server cannot locate ' + req.url);
+// Error handling
+app.use((req, res, next) => {
+    const err = new Error('The server cannot locate ' + req.url);
     err.status = 404;
     next(err);
 });
